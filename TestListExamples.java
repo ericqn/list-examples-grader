9406e04a1b3afe1513e644f8e6130e96b2b87467
ericqn <ericqn1234@gmail.com>
2022-11-11 15:35:42 -0800

    created bash file to grade programming assignments

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,64 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test 
+	public void testMerge() {
+    List<String> input = new ArrayList<>();
+
+    input.add("a");
+    input.add("b");
+    input.add("c");
+    input.add("d");
+
+    List<String> input2 = new ArrayList<>();
+
+    input2.add("e");
+    input2.add("f");
+
+    List<String> inputMod = new ArrayList<>();
+
+    inputMod.add("a");
+    inputMod.add("b");
+    inputMod.add("c");
+    inputMod.add("d");
+    inputMod.add("e");
+    inputMod.add("f");
+
+    assertEquals(inputMod, ListExamples.merge(input, input2));
+  }
+
+  @Test 
+	public void testFilter() {
+    List<String> input = new ArrayList<>();
+    StringChecker boom = new ChecksBomb();
+
+    input.add("kabOMb");
+    input.add("bobby");
+    input.add("apple");
+    input.add("bomb");
+
+    List<String> inputMod = new ArrayList<>();
+
+    inputMod.add("kabOMb");
+    inputMod.add("bomb");
+
+    assertEquals(inputMod, ListExamples.filter(input, boom));
+  }
+
 }
+
+class ChecksBomb implements StringChecker{
+  public boolean checkString(String s) {
+    if(s.toLowerCase().contains("bomb")) {
+      return true;
+    }
+    return false;
+  }
+}
+
--- grade.sh
+++ grade.sh
@@ -1,7 +1,48 @@
 # Create your grading script here
 
-set -e
+# set -e
+
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
+cp TestListExamples.java student-submission/
 
+cp -r lib/ student-submission/lib/
+
+cd student-submission
+
+# checks to see if ListExamples.java exists in student-submission
+if [[ -e "ListExamples.java" ]] 
+then 
+    echo "file exists." 
+else 
+    echo "ListExamples.java does not exist." 
+fi
+
+ # check to see if it compiles
+javac ListExamples.java
+
+if [[ $? -eq 0 ]]
+then 
+    echo "Compile success!" 
+else 
+    echo "Compile error." 
+fi
+
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > fileOutput.txt
+
+grep "FAILURES!!!" fileOutput.txt > fail.txt
+grep "OK" fileOutput.txt > correct.txt
+
+if [[ $? -eq 0 ]]
+then
+    echo "score: 2/2"
+else
+    echo ""
+fi
+
+echo "grader finished."
+exit
